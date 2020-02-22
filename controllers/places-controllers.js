c1a870bf80eb61d77abc2b015ca4c56f52a7d782
Jad K <jad_dis@hotmail.com>
2020-02-22 01:09:09 -0500

    added a POST route to places and its requisite controller function

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,5 @@
 const HttpError = require("../models/http-error");
-
+const uuid = require("uuid/v4");
 const DUMMY_PLACES = [
   {
     id: "p1",
@@ -39,4 +39,21 @@ const getPlaceByUserId = (req, res, next) => {
 
   res.json({ place });
 }
-module.exports = { getPlaceById, getPlaceByUserId };
+
+const createPlace = (req, res, next) => {
+ const { title, description, coordinates, address, creator} = req.body;
+ 
+ const createdPlace = {
+   id: uuid(),
+   title,
+   description,
+   location: coordinates,
+   address,
+   creator
+ }
+
+ DUMMY_PLACES.push(createdPlace);
+ res.status(201).json({ place: createdPlace});
+}
+
+module.exports = { getPlaceById, getPlaceByUserId, createPlace };
