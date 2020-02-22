870e8edbd62fec3c6b5bc749c644f9d250e8e0e9
Jad K <jad_dis@hotmail.com>
2020-02-21 22:04:01 -0500

    added a place route for accessing places by id and setup up some dummy data for testing purposes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("GET Request in Places");
-  res.json({message: "GET Request in Places received"})
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: "20 W 34th ST, New York, NY 10001",
+    creator: "u1"
+  }
+]
+router.get("/:pid", (req, res) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId)
+  res.json({ place })
 });
 
 module.exports = router;
