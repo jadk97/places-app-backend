0b3f5b1f6dfdda969dbbc86d196de70da3803daa
Jad K <jad_dis@hotmail.com>
2020-02-21 23:26:50 -0500

    added some more routes to the places endpoint and implemented some basic error handling

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -15,10 +15,30 @@ const DUMMY_PLACES = [
     creator: "u1"
   }
 ]
-router.get("/:pid", (req, res) => {
+router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId)
-  res.json({ place })
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for that provided id.");
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ place });
 });
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for that provided user id.");
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ place });
+})
 
 module.exports = router;
