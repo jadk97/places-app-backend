9540ef3dbb0b14696943b05ee5a2388e4b8dd659
Jad K <jad_dis@hotmail.com>
2020-02-22 02:51:58 -0500

    added input validaton to PATCH route in places

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -43,7 +43,7 @@ const getPlacesByUserId = (req, res, next) => {
 
 const createPlace = (req, res, next) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
+  if (!errors.isEmpty()) {
     const error = new HttpError("Invalid inputs passed, please check your data.", 422);
     return next(error);
   }
@@ -63,6 +63,11 @@ const createPlace = (req, res, next) => {
 }
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(error);
+  }
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -81,7 +86,7 @@ const updatePlace = (req, res, next) => {
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
-  res.status(200).json({message: `Deleted place with id: ${placeId}`});
+  res.status(200).json({ message: `Deleted place with id: ${placeId}` });
 }
 
 module.exports = {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -17,7 +17,11 @@ check("address").not().isEmpty(),
 ]
 ,placesControllers.createPlace);
 
-router.patch("/:pid", placesControllers.updatePlace);
+router.patch("/:pid", 
+[check("title").not().isEmpty(), 
+check("description").isLength({min: 5})
+],
+placesControllers.updatePlace);
 
 router.delete("/:pid", placesControllers.deletePlace);
 
