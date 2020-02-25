a025ef14c6eadaf77c01d0218ab83673d27a6ee0
Jad K <jad_dis@hotmail.com>
2020-02-25 03:14:00 -0500

    created a Place model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 
+
 const url = "YOUR_API_KEY";
 
 const placesRoutes = require("./routes/places-routes");
