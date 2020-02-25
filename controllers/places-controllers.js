f643a611afef99529f39e13b8ffe34de9b756226
Jad K <jad_dis@hotmail.com>
2020-02-25 04:24:25 -0500

    refactored getPlacesByUserId to use mongoose and the database

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -39,16 +39,24 @@ const getPlaceById = async (req, res, next) => {
 
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
+  let places;
+
+  try {
+    places = await Place.find({ creator: userId })
+  }
+  catch (err) {
+    const error = new HttpError("Fetching places failed, please try again later.", 500);
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
     const error = new HttpError("Could not find a place for that provided user id.", 404);
     return next(error);
   }
 
-  res.json({ places });
+  res.json({ places: places.map(place => place.toObject({ getters: true })) });
 }
 
 const createPlace = async (req, res, next) => {
