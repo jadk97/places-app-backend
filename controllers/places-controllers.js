26944628db2bfe77fe7193697a624b6a8b548417
Jad K <jad_dis@hotmail.com>
2020-02-25 04:30:12 -0500

    refactored updatePlace to make use of mongoose and the database

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -95,7 +95,7 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 }
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new HttpError("Invalid inputs passed, please check your data.", 422);
@@ -104,16 +104,27 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  let place;
+  try{
+    place = await Place.findById(placeId);
+  }
+  catch(err){
+    const error = new HttpError("Something went wrong, could not update place", 500);
+    return next(error);
+  }
 
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  place.title = title;
+  place.description = description;
 
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  try{
+    await place.save();
+  }
+  catch(err){
+    const error = new HttpError("Something went wrong, could not update place", 500);
+    return next(error);
+  }
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
-
-  res.status(200).json({ place: updatedPlace })
+  res.status(200).json({ place: place.toObject({getters: true}) });
 }
 
 const deletePlace = (req, res, next) => {
