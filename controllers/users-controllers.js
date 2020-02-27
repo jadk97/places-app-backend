46bf2b98fc024104fba07cd08d48893edb08d239
Jad K <jad_dis@hotmail.com>
2020-02-26 23:25:36 -0500

    defined a relationship between the User and Place models

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -31,7 +31,7 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  const { name, email, password, places } = req.body;
+  const { name, email, password } = req.body;
 
   let existingUser;
   try {
@@ -53,7 +53,7 @@ const signup = async (req, res, next) => {
     email,
     image: "https://randomuser.me/api/portraits/men/18.jpg",
     password,
-    places
+    places: []
   });
 
   try {
