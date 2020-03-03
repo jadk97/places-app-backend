43b524604421ed0eba368948bc0e04bb0186be85
Jad K <jad_dis@hotmail.com>
2020-03-03 03:06:43 -0500

    modified the JWT token

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -71,7 +71,7 @@ const signup = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
-      "tempsecret",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   }
@@ -125,7 +125,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      "tempsecret",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   }
