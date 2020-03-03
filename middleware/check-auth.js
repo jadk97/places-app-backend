eb3df53b6c44a59ab877df4c5511839cfed244e6
Jad K <jad_dis@hotmail.com>
2020-03-03 01:12:25 -0500

    sighlty tweaked the auth middleware to play nice with the front-end

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,9 +2,12 @@ const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(" ")[1];
-    
+
     if (!token) {
       throw new Error("Authentication failed!");
     }
